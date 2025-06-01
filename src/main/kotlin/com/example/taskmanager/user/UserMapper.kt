4ac38bb87c07b70faa75a5fe9901a5fa52547211
MwirigiMuriithi <learnerviner@gmail.com>
2025-06-01 14:30:42 +0300

    Adjust UserMapper to handle new fields and transformations

--- src/main/kotlin/com/example/taskmanager/user/UserMapper.kt
+++ src/main/kotlin/com/example/taskmanager/user/UserMapper.kt
@@ -2,9 +2,19 @@
 package com.example.taskmanager.user
 
 import org.mapstruct.Mapper
+import org.mapstruct.Mapping
+
+// @Mapper(componentModel = "spring")
+// interface UserMapper {
+//     fun toDto(user: User): UserDTO         // Convert Entity to DTO
+//     fun toEntity(userDTO: UserDTO): User   // Convert DTO to Entity
+// }
 
 @Mapper(componentModel = "spring")
 interface UserMapper {
-    fun toDto(user: User): UserDTO         // Convert Entity to DTO
-    fun toEntity(userDTO: UserDTO): User   // Convert DTO to Entity
+    @Mapping(target = "password", ignore = true)
+    fun toEntity(userDto: UserDTO): User
+
+    fun toDto(user: User): UserDTO
 }
+
