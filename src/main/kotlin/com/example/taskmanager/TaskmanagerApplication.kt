51988de65a86c52b503c7d22bc7804661125a76d
MwirigiMuriithi <learnerviner@gmail.com>
2025-06-01 11:09:35 +0300

    Update application entry point

--- src/main/kotlin/com/example/taskmanager/TaskmanagerApplication.kt
+++ src/main/kotlin/com/example/taskmanager/TaskmanagerApplication.kt
@@ -2,10 +2,20 @@ package com.example.taskmanager
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import io.github.cdimascio.dotenv.dotenv
 
 @SpringBootApplication
 class TaskmanagerApplication
 
 fun main(args: Array<String>) {
-	runApplication<TaskmanagerApplication>(*args)
+    // Load the .env file into environment variables
+    val dotenv = dotenv {
+        ignoreIfMissing = true // allows running in prod without .env
+    }
+
+    dotenv.entries().forEach {
+        System.setProperty(it.key, it.value)
+    }
+
+    runApplication<TaskmanagerApplication>(*args)
 }
