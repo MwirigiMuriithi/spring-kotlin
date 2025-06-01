f8749c671a2f928572bd00baa879c4276cf3fb88
MwirigiMuriithi <learnerviner@gmail.com>
2025-06-01 10:57:33 +0300

    Add User repository

---
+++ src/main/kotlin/com/example/taskmanager/user/UserRepository.kt
@@ -0,0 +1,8 @@
+// src/main/kotlin/com/example/taskmanager/user/UserRepository.kt
+package com.example.taskmanager.user
+
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface UserRepository : JpaRepository<User, Long>
