ea19c9afe4ee65306353c531fa628e0c4ddaf486
MwirigiMuriithi <learnerviner@gmail.com>
2025-06-02 12:58:51 +0300

    Modified TaskRepository.kt to query by user ownership

--- src/main/kotlin/com/example/taskmanager/task/TaskRepository.kt
+++ src/main/kotlin/com/example/taskmanager/task/TaskRepository.kt
@@ -1,8 +1,12 @@
 // src/main/kotlin/com/example/taskmanager/task/TaskRepository.kt
 package com.example.taskmanager.task
 
+import com.example.taskmanager.user.User
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface TaskRepository : JpaRepository<Task, Long>
+interface TaskRepository : JpaRepository<Task, Long> {
+    fun findAllByUser(user: User): List<Task>
+    fun findByIdAndUser(id: Long, user: User): Task?
+}
