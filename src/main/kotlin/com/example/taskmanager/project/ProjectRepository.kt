b06a1c5112a09eaa368c204d0c0bdb8ca61b47a4
MwirigiMuriithi <learnerviner@gmail.com>
2025-06-02 12:58:51 +0300

    Updated ProjectRepository.kt to support owner-based queries

--- src/main/kotlin/com/example/taskmanager/project/ProjectRepository.kt
+++ src/main/kotlin/com/example/taskmanager/project/ProjectRepository.kt
@@ -1,8 +1,12 @@
 // src/main/kotlin/com/example/taskmanager/project/ProjectRepository.kt
 package com.example.taskmanager.project
 
+import com.example.taskmanager.user.User
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface ProjectRepository : JpaRepository<Project, Long>
+interface ProjectRepository : JpaRepository<Project, Long> {
+    fun findAllByUser(user: User): List<Project>
+    fun findByIdAndUser(id: Long, user: User): Project?
+}
