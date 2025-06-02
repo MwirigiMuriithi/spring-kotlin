eaa804b87ac2cf653010e7487822b80dce3ce869
MwirigiMuriithi <learnerviner@gmail.com>
2025-06-02 12:58:51 +0300

    Updated TaskDTO.kt to carry ownership metadata

--- src/main/kotlin/com/example/taskmanager/task/TaskDTO.kt
+++ src/main/kotlin/com/example/taskmanager/task/TaskDTO.kt
@@ -1,9 +1,9 @@
 // src/main/kotlin/com/example/taskmanager/task/TaskDTO.kt
 package com.example.taskmanager.task
 
-// DTO for Task data transfer
 data class TaskDTO(
     val id: Long = 0,
     val title: String,
-    val description: String
+    val description: String,
+    // val status: TaskStatus = TaskStatus.TODO
 )
